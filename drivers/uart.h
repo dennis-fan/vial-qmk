23dd0644bb4f1772e30305acd9be0ff2ff16bb69
Dennis Fan <dennisfan@gmail.com>
2025-08-04 00:27:13 -0700

    update configuration to test no wake from sleep fix

--- drivers/uart.h
+++ drivers/uart.h
@@ -6,6 +6,145 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include <hal.h>
+
+#include "gpio.h"
+#include "chibios_config.h"
+
+// ======== DEPRECATED DEFINES - DO NOT USE ========
+#ifdef SERIAL_DRIVER
+#    define UART_DRIVER SERIAL_DRIVER
+#endif
+#ifdef SD1_TX_PIN
+#    define UART_TX_PIN SD1_TX_PIN
+#endif
+#ifdef SD1_RX_PIN
+#    define UART_RX_PIN SD1_RX_PIN
+#endif
+#ifdef SD1_CTS_PIN
+#    define UART_CTS_PIN SD1_CTS_PIN
+#endif
+#ifdef SD1_RTS_PIN
+#    define UART_RTS_PIN SD1_RTS_PIN
+#endif
+#ifdef SD1_TX_PAL_MODE
+#    define UART_TX_PAL_MODE SD1_TX_PAL_MODE
+#endif
+#ifdef SD1_RX_PAL_MODE
+#    define UART_RX_PAL_MODE SD1_RX_PAL_MODE
+#endif
+#ifdef SD1_CTS_PAL_MODE
+#    define UART_RTS_PAL_MODE SD1_CTS_PAL_MODE
+#endif
+#ifdef SD1_RTS_PAL_MODE
+#    define UART_TX_PAL_MODE SD1_RTS_PAL_MODE
+#endif
+#ifdef SD1_CR1
+#    define UART_CR1 SD1_CR1
+#endif
+#ifdef SD1_CR2
+#    define UART_CR2 SD1_CR2
+#endif
+#ifdef SD1_CR3
+#    define UART_CR3 SD1_CR3
+#endif
+#ifdef SD1_WRDLEN
+#    define UART_WRDLEN SD1_WRDLEN
+#endif
+#ifdef SD1_STPBIT
+#    define UART_STPBIT SD1_STPBIT
+#endif
+#ifdef SD1_PARITY
+#    define UART_PARITY SD1_PARITY
+#endif
+#ifdef SD1_ATFLCT
+#    define UART_ATFLCT SD1_ATFLCT
+#endif
+// ========
+
+#ifndef UART_DRIVER
+#    if (HAL_USE_SERIAL == TRUE)
+#        define UART_DRIVER SD1
+#    elif (HAL_USE_SIO == TRUE)
+#        define UART_DRIVER SIOD1
+#    endif
+#endif
+
+#ifndef UART_TX_PIN
+#    define UART_TX_PIN A9
+#endif
+
+#ifndef UART_RX_PIN
+#    define UART_RX_PIN A10
+#endif
+
+#ifndef UART_CTS_PIN
+#    define UART_CTS_PIN A11
+#endif
+
+#ifndef UART_RTS_PIN
+#    define UART_RTS_PIN A12
+#endif
+
+#ifdef USE_GPIOV1
+#    ifndef UART_TX_PAL_MODE
+#        define UART_TX_PAL_MODE PAL_MODE_ALTERNATE_PUSHPULL
+#    endif
+#    ifndef UART_RX_PAL_MODE
+#        define UART_RX_PAL_MODE PAL_MODE_INPUT
+#    endif
+#    ifndef UART_CTS_PAL_MODE
+#        define UART_CTS_PAL_MODE PAL_MODE_INPUT
+#    endif
+#    ifndef UART_RTS_PAL_MODE
+#        define UART_RTS_PAL_MODE PAL_MODE_ALTERNATE_PUSHPULL
+#    endif
+#else
+#    ifndef UART_TX_PAL_MODE
+#        define UART_TX_PAL_MODE 7
+#    endif
+
+#    ifndef UART_RX_PAL_MODE
+#        define UART_RX_PAL_MODE 7
+#    endif
+
+#    ifndef UART_CTS_PAL_MODE
+#        define UART_CTS_PAL_MODE 7
+#    endif
+
+#    ifndef UART_RTS_PAL_MODE
+#        define UART_RTS_PAL_MODE 7
+#    endif
+#endif
+
+#ifndef UART_CR1
+#    define UART_CR1 0
+#endif
+
+#ifndef UART_CR2
+#    define UART_CR2 0
+#endif
+
+#ifndef UART_CR3
+#    define UART_CR3 0
+#endif
+
+#ifndef UART_WRDLEN
+#    define UART_WRDLEN 3
+#endif
+
+#ifndef UART_STPBIT
+#    define UART_STPBIT 0
+#endif
+
+#ifndef UART_PARITY
+#    define UART_PARITY 0
+#endif
+
+#ifndef UART_ATFLCT
+#    define UART_ATFLCT 0
+#endif
+
 /**
  * \file
  *
